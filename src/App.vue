2959aa166dfeba6bd3a5e431a446c6d8f9fedbca
jiuer12138 <18815283494@163.com>
2022-06-28 20:57:58 +0800

    实现百度换肤

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,54 @@
 <template>
-  <div>
-    <div>
-      <span>用户名:</span>
-      <input type="text" v-model.trim="username" />
+  <div class="container">
+    <div class="imglist">
+      <h3>图片列表区</h3>
+      <img src="./assets/01.jpg" alt="" @mouseover="fn(0)" @click="btn(0)"/>
+      <img src="./assets/02.jpg" alt="" @mouseover="fn(1)" @click="btn(1)"/>
+      <img src="./assets/03.jpg" alt="" @mouseover="fn(2)" @click="btn(2)"/>
     </div>
-    <div>
-      <span>密码:</span>
-      <input type="password" v-model.number="pass" />
+    <div class="show">
+      <h3>图片预览区</h3>
+      <img :src="imgUrl" alt="" style="width: 600px; height: auto" />
     </div>
-    <div>
-      <span>来自于: </span>
-      <select v-model="from">
-        <option value="北京市">北京</option>
-        <option value="南京市">南京</option>
-        <option value="天津市">天津</option>
-      </select>
-    </div>
-    <div>
-      <span>爱好: </span>
-      <input type="checkbox" v-model="hobby" value="抽烟" />抽烟
-      <input type="checkbox" v-model="hobby" value="喝酒" />喝酒
-      <input type="checkbox" v-model="hobby" value="写代码" />写代码
-    </div>
-    <div>
-      <span>性别: </span>
-      <input type="radio" value="男" name="sex" v-model="sex" />男
-      <input type="radio" value="女" name="sex" v-model="sex" />女
-    </div>
-    <div>
-      <span>自我介绍</span>
-      <textarea v-model.lazy="intro"></textarea>
-    </div>
+    <div></div>
   </div>
 </template>
 
 <script>
+import img1 from './assets/01.jpg'
+import img2 from './assets/02.jpg'
+import img3 from './assets/03.jpg'
 export default {
+  name: 'App',
   data() {
     return {
-      username: '',
-      pass: '',
-      from: '北京市',
-      hobby: ['抽烟'],
-      sex: '男',
-      intro: '',
+      imgUrl: img1,
+    }
+  },
+  methods: {
+    fn(val) {
+      return (this.imgUrl = [img1, img2, img3][val])
+    },
+    btn(val){
+      return alert(`图片是：第${val+1}张，地址是：${[img1, img2, img3][val]}`)
     }
   },
 }
 </script>
+
+<style scoped>
+.container {
+  display: flex;
+}
+.imglist {
+  border: 2px solid red;
+}
+.imglist img {
+  width: 200px;
+  height: auto;
+}
+.show {
+  border: 2px solid red;
+  margin-right: 20px;
+}
+</style>
