ab4174d457f67d88e9c63a49fd4ff36694525329
jiuer12138 <18815283494@163.com>
2022-06-28 20:45:09 +0800

    v-model和修饰符

---
+++ src/翻转字母.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+    <button @click="btn">逆转世界</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      message: 'Hello, world!',
+    }
+  },
+  methods: {
+    btn() {
+      return (this.message = [...this.message].reverse().join(''))
+    },
+  },
+}
+</script>
+
+<style></style>
