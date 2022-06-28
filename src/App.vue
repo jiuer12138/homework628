8d6d4b25460151702fcdf3e0bb2cbfedccbb4709
jiuer12138 <18815283494@163.com>
2022-06-28 20:37:47 +0800

    课上练习-翻转世界

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>{{ message }}</h1>
+    <button @click="btn">逆转世界</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      message: 'Hello, world!',
+    }
+  },
+  methods: {
+    btn() {
+      return (this.message = [...this.message].reverse().join(''))
+    },
+  },
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
